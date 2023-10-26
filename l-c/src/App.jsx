9b4384b4033cb552f4b61abb3551f071440c70d6
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2023-10-26 17:43:14 +0200

    ajout du logo

--- l-c/src/App.jsx
+++ l-c/src/App.jsx
@@ -1,13 +1,17 @@
 import "antd/dist/antd";
 import { Menu } from "antd";
-import { WrapperMenu, Logo } from "./components/WrapperMenu.tsx";
+import { WrapperMenu, Logo, LogoTitle } from "./components/WrapperMenu.tsx";
 
 function App() {
   return (
     <div className="App">
       <WrapperMenu>
-            <Logo />
+            <Logo>
+              <LogoTitle> Licornes & Clémentines</LogoTitle>
+            </Logo>
             <Menu 
+            style={{ backgroundColor:"#C9156B", color:"white", fontSize:"18px"}}
+            colorItemHoverBg={"rgba(66,135,245,0)"}
             items={[
               { label: "Soirée", key: "soirée"},
               { label: "Membre", key: "membre"},

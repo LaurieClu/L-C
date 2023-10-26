6f793d2e1007317bb0f51803cfb0d96f7de6ed63
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2023-10-26 15:37:39 +0200

    config typescript

---
+++ l-c/src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const Menu = () => {
+    return (
+        <div>
+            <p>
+            TOTOTO salut c'est nous
+            </p>
+        </div>
+    )
+}
+
+export default Menu; 

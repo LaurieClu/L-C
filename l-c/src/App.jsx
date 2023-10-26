35a8220957ab58865b64826d6e11c79b93043726
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2023-10-26 16:42:09 +0200

    first step menu

--- l-c/src/App.jsx
+++ l-c/src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css';
-import  Menu from './components/Menu';
+import "antd/dist/antd";
+import { Menu } from "antd";
+import { WrapperMenu, Logo } from "./components/WrapperMenu.tsx";
 
 function App() {
   return (
     <div className="App">
-        <Menu />
+      <WrapperMenu>
+            <Logo />
+            <Menu 
+            items={[
+              { label: "Soirée", key: "soirée"},
+              { label: "Membre", key: "membre"},
+              { label: "Calendrier", key: "calendrier"},
+              { label: "Inventaire", key: "inventaire"},
+              { label: "Courses", key: "courses"},
+              { label: "Modèles Messages", key: "modele-message"},
+              { label: "Budget", key: "budget"},
+              { label: "Utiles", key: "utiles"}
+            ]}
+            />
+        </WrapperMenu>
     </div>
   );
 }
---
+++ l-c/src/components/WrapperMenu.tsx
@@ -0,0 +1,16 @@
+import styled  from "styled-components";
+
+export const WrapperMenu = styled.div`
+	display: flex;
+    max-width: 20%;
+	width: 100%;
+	height: 100vh;;
+    text-align: center;
+    align-content: space-between;
+    flex-direction: column;
+`;
+
+export const Logo = styled.div`
+    height: 10%;
+    background-color: blue;
+`;

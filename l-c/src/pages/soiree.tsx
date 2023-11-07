06a25f26d27560dd198e4f7829a773e8c3b8b2f5
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2023-11-07 23:23:20 +0100

    quick fix before dodo

--- l-c/src/pages/soiree.tsx
+++ l-c/src/pages/soiree.tsx
@@ -1,78 +1,85 @@
 import * as React from "react";
 import { Table, Tag } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import { WrapperContainer } from "../styles/Soiree";
+import { WrapperContainer, Date } from "../styles/Soiree";
 
-interface DataType {
+export interface DataType {
     key: string;
     name: string;
     statut: number;
     paiement: string;
-    annulation : string[];
+    annulation: string[];
 }
 
-const columns: ColumnsType<DataType> = [
+export const columns: ColumnsType<DataType> = [
     {
-    title: 'Prénom',
-    dataIndex: 'name',
-    key: 'name',
+        title: 'Prénom',
+        dataIndex: 'name',
+        key: 'name',
     },
     {
-    title: 'Statut',
-    dataIndex: 'statut',
-    key: 'statut',
+        title: 'Statut',
+        dataIndex: 'statut',
+        key: 'statut',
     },
     {
-    title: 'Paiement',
-    dataIndex: 'paiement',
-    key: 'paiement',
+        title: 'Paiement',
+        dataIndex: 'paiement',
+        key: 'paiement',
     },
     {
-    title: 'Annulation',
-    key: 'annulation',
-    dataIndex: 'annulation',
-    render: (_, { annulation }) => (
-        <>
-        {annulation.map((annulation) => {
-            let color = annulation.length > 5 ? 'geekblue' : 'green';
-            if (annulation === 'loser') {
-            color = 'volcano';
-            }
-            return (
-            <Tag color={color} key={annulation}>
-                {annulation.toUpperCase()}
-            </Tag>
-            );
-        })}
-        </>
-    ),
-    },
-];
-
-const data: DataType[] = [
-    {
-    key: '1',
-    name: 'John Brown',
-    statut: 32,
-    paiement: 'New York No. 1 Lake Park',
-    annulation: ['nice', 'developer'],
-    },
-    {
-    key: '2',
-    name: 'Jim Green',
-    statut: 42,
-    paiement: 'London No. 1 Lake Park',
-    annulation: ['loser'],
-    },
-    {
-    key: '3',
-    name: 'Joe Black',
-    statut: 32,
-    paiement: 'Sydney No. 1 Lake Park',
-    annulation: ['cool', 'teacher'],
+        title: 'Annulation',
+        key: 'annulation',
+        dataIndex: 'annulation',
+        render: (_, { annulation }) => (
+            <>
+                {annulation.map((annulation) => {
+                    let color = annulation.length > 5 ? 'geekblue' : 'green';
+                    if (annulation === 'loser') {
+                        color = 'volcano';
+                    }
+                    return (
+                        <Tag color={color} key={annulation}>
+                            {annulation.toUpperCase()}
+                        </Tag>
+                    );
+                })}
+            </>
+        ),
     },
 ];
 
-const Soiree: React.FC = () => <WrapperContainer><Table columns={columns} dataSource={data} /></WrapperContainer>;
+export const data: DataType[] = [
+    {
+        key: '1',
+        name: 'John Brown',
+        statut: 32,
+        paiement: 'New York No. 1 Lake Park',
+        annulation: ['nice', 'developer'],
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        statut: 42,
+        paiement: 'London No. 1 Lake Park',
+        annulation: ['loser'],
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        statut: 32,
+        paiement: 'Sydney No. 1 Lake Park',
+        annulation: ['cool', 'teacher'],
+    },
+];
+
+const Soiree = () => {
+    return (
+        <WrapperContainer>
+            {/* <Date>{setDate}</Date> */}
+            <Table columns={columns} dataSource={data} />
+        </WrapperContainer>
+    );
+}
 
 export default Soiree;
--- l-c/src/styles/Soiree.tsx
+++ l-c/src/styles/Soiree.tsx
@@ -9,3 +9,13 @@ margin-left: -50px;
 width: 100px;
 height: 100px;
 `;
+
+export const Date = styled.div`
+position: absolute;
+top: 25%;
+left: 25%;
+margin-top: -50px;
+margin-left: -50px;
+width: 100px;
+height: 100px;
+`;

2868fa0656592af1cd7d13612c123232703317f8
GOnzalez de Canales Laura <lgonzdc@gmail.com>
2023-11-11 19:36:42 +0100

    conditional rendered adress

--- l-c/src/components/Airbnb.tsx
+++ l-c/src/components/Airbnb.tsx
@@ -14,7 +14,7 @@ export const Airbnb= () =>{
     const [adress, setAdress] = useState (() => {
          // getting stored value
         const saved = localStorage.getItem("adress");
-        return saved || "";
+        return saved || "Entrer adresse";
     });
 
 
@@ -33,18 +33,20 @@ export const Airbnb= () =>{
                     (e) => {
                       localStorage.setItem('airbnbCheckbox',`${e.target.checked}`);
                       setChecked(e.target.checked);
-                      console.log(e.target.checked)
                     }
                   }>  Trouvé
                 </Checkbox>
                 <div style={{paddingTop:"15px"}}>
-            
+
+                  {isChecked ?
                     <TextArea 
-                        value = {adress} 
-                        onChange={(e:any) => setAdress(e.target.value)} 
-                        rows={4} 
-                        placeholder="Entrer adresse"/> 
-                    
+                    value = {adress} 
+                    onChange={(e:any) => setAdress(e.target.value)} 
+                    rows={4} 
+                    placeholder="Entrer adresse"/>
+                    : null
+                    }
+                     
                 </div>
                 
             </Card>

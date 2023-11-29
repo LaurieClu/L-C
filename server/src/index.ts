4ac1c55cb4223e2839e5bc87eb15ff3a7054bc7e
GOnzalez de Canales Laura <lgonzdc@gmail.com>
2023-11-29 17:53:56 +0100

    config backend + dotenv to secure db connexion

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,20 @@
 import express from "express"
+import mongoose from "mongoose"
+import {config} from "dotenv"
+config()
 
-const app = express ()
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+    console.log(`listening on port ${PORT}`);
+    app.listen(PORT)
+})
 
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
 
-app.listen(5000)
